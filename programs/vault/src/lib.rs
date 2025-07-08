69466b2c47e030f7c31cb72dd2e62d85568caa59
Gaurav <burandegaurav8899@gmail.com>
2025-07-08 23:46:08 +0530

    feat: initialize vault_state pda with data

---
+++ programs/vault/src/lib.rs
@@ -0,0 +1,57 @@
+use anchor_lang::prelude::*;
+
+declare_id!("78MSmCkLMMh3LhrrWFRHRB3gPDSLRqLDkQALDmwqQ3rc");
+
+
+#[program]
+pub mod vault {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"state", user.key().as_ref()],
+        bump,
+        space =  8 + VaultState::INIT_SPACE
+    )]
+    pub vault_state: Account<'info, VaultState>,
+
+    #[account(
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+impl Initialize<'_> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vault_state.vault_bump = bumps.vault;
+        self.vault_state.state_bump = bumps.vault_state;
+
+        Ok(())
+    }
+}
+
+
+#[account]
+pub struct VaultState {
+    pub vault_bump: u8,
+    pub state_bump: u8
+}
+
+impl Space for VaultState {
+    const INIT_SPACE:usize = 1 + 1;
+}
