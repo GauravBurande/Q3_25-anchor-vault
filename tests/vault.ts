c718b5c5c658f6f40dc95f441e8d5f25296a209b
Gaurav <burandegaurav8899@gmail.com>
2025-07-09 15:21:16 +0530

    feat: added tests for vault program

--- tests/vault.ts
+++ tests/vault.ts
@@ -4,13 +4,92 @@ import { Vault } from "../target/types/vault";
 
 describe("vault", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.vault as Program<Vault>;
 
+  const [vaultState, vaultStateBump] =
+    anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("state"), provider.wallet.publicKey.toBytes()],
+      program.programId
+    );
+
+  const [vault, vaultBump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), provider.wallet.publicKey.toBytes()],
+    program.programId
+  );
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods
+      .initialize()
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vault,
+        vaultState,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log("\nYour transaction signature", tx);
+    console.log(
+      "Your vault info",
+      await provider.connection.getAccountInfo(vault)
+    );
+  });
+
+  it("Deposits 2 sol", async () => {
+    const tx = await program.methods
+      .deposit(new anchor.BN(2 * anchor.web3.LAMPORTS_PER_SOL))
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vault,
+        vaultState,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log("\nYour transaction signature", tx);
+    console.log(
+      "Your vault info",
+      await provider.connection.getAccountInfo(vault)
+    );
+  });
+
+  it("Withdraws 1 sol", async () => {
+    const tx = await program.methods
+      .withdraw(new anchor.BN(1 * anchor.web3.LAMPORTS_PER_SOL))
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vault,
+        vaultState,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log("\nYour transaction signature", tx);
+    console.log(
+      "Your vault info",
+      await provider.connection.getAccountInfo(vault)
+    );
+  });
+
+  it("closes the vault", async () => {
+    const tx = await program.methods
+      .close()
+      .accountsPartial({
+        user: provider.wallet.publicKey,
+        vault,
+        vaultState,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log("\nYour transaction signature", tx);
+    console.log(
+      "Your vault info",
+      await provider.connection.getAccountInfo(vault)
+    );
   });
 });
