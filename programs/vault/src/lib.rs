1a8d84e66a7a500b8175f51906d82d20fab8de2e
Gaurav <burandegaurav8899@gmail.com>
2025-07-09 21:31:44 +0530

    fix: seeds for vault

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -38,7 +38,7 @@ pub struct Initialize<'info> {
     pub vault_state: Account<'info, VaultState>,
 
     #[account(
-        seeds = [b"vault", vault_state.key().as_ref()],
+        seeds = [b"vault", user.key().as_ref()],
         bump
     )]
     pub vault: SystemAccount<'info>,
